35e11fdba23e413fef9066b8e9ce13c433b818d9
Roz1204 <lifeiswanderful1234@gmail.com>
2026-06-01 21:39:40 +0400

    Added language button

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,15 +1,31 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
 
 export default function Navbar() {
+    const [language, setLanguage] = useState("Հայ");
+
+    const toggleLanguage = () => {
+        setLanguage(prev =>
+            prev === "Հայ" ? "ENG" : "Հայ"
+        );
+    };
+
     return (
         <nav className="navbar">
 
             <Link to="/" className="logo">
-                NewTax.am
+                Tax.am
             </Link>
 
             <div className="nav-buttons">
+                <button
+                    className="language-btn"
+                    onClick={toggleLanguage}
+                >
+                    🌐 {language}
+                </button>
 
                 <button className="login-btn">
                     Login
