2886076864adc69afe79b10cbb8eb27f6cb8662d
Roz1204 <lifeiswanderful1234@gmail.com>
2026-06-08 21:41:26 +0400

    Hashing added

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,46 +1,3 @@
-# from fastapi import FastAPI, HTTPException
-# from fastapi.middleware.cors import CORSMiddleware
-# from pydantic import BaseModel, EmailStr
-
-# app = FastAPI()
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["http://localhost:5173"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# fake_db = []
-
-# class SignupRequest(BaseModel):
-#     name: str
-#     email: EmailStr
-#     password: str
-#     confirm_password: str
-
-
-# @app.post("/signup")
-# def signup(user: SignupRequest):
-#     print(user.dict())
-#     if user.password != user.confirm_password:
-#         raise HTTPException(status_code=400, detail="Գաղտնաբառերը չեն համընկնում")
-
-#     for u in fake_db:
-#         if u["email"] == user.email:
-#             raise HTTPException(status_code=400, detail="Այս email-ը արդեն օգտագործված է")
-
-#     fake_db.append({
-#         "name": user.name,
-#         "email": user.email,
-#         "password": user.password
-#     })
-
-#     return {"message": "User registered successfully"}
-
-
-
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
@@ -49,6 +6,8 @@ from app.database import SessionLocal, engine
 from app.models import User
 from app.schemas import SignupRequest
 from app.database import Base
+
+from app.security import hash_password
 
 Base.metadata.create_all(bind=engine)
 
@@ -93,10 +52,12 @@ def signup(user: SignupRequest, db: Session = Depends(get_db)):
             detail="Այս email-ը արդեն օգտագործված է"
         )
 
+    hashed_pw = hash_password(user.password)
+
     new_user = User(
         name=user.name,
         email=user.email,
-        password=user.password
+        password=hashed_pw
     )
 
     db.add(new_user)
